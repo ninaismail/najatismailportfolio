407a047810d4cfcb344d73a327ed7b2a2d08cdc5
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-02-15 23:25:25 +0200

    returned the temp layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import About from "./components/About"
 import Banner from "./components/Banner"
-import GridItems from "./components/BentoDesign/GridItems"
 import Contact from "./components/Contact"
 import Projects from "./components/Projects"
 
@@ -8,7 +7,10 @@ function App() {
 
   return (
   <main className="font-primary">
-    <GridItems/>
+    <Banner/>
+    <About/>
+    <Projects/>
+    <Contact/>
   </main>
   )
 }
